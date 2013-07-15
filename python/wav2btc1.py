70a1f7bc81abd1fde7b4170b5f35184341fb27e1
Luis Panadero Guardeño <luis.panadero@gmail.com>
2013-07-15 20:15:59 +0200

    Parcial rewrite to support librarys intead of only single files. Remove RAW BIN for the moment. IHEX now exports BTLto IHEX. TODO BTL itselfs

--- python/wav2btc1.py
+++ python/wav2btc1.py
@@ -10,14 +10,14 @@
 
 from __future__ import division
 
-VERSION = '0.1b'
+VERSION = '0.2'
 
 CHUNK = 1024        # How many samples send to player
 BYTES = 2           # N bytes arthimetic
 MAX = 2**(BYTES*8 -1) -1
 MIN = -(2**(BYTES*8 -1)) +1   
 
-COLUMN = 12         # Prety print of values
+COLUMN = 8         # Prety print of values
 
 
 import sys
@@ -26,20 +26,244 @@ import os.path
 
 import array
 from math import log, exp, floor, ceil
-import fpformat
 
 import pyaudio
 import wave
 import audioop
 
-def ReadWAV (file):
-  """Reads a wave file and return sample rate and mono audio data """
+
+def CArrayPrint (bytedata, f, **options):
+    """ Prints a Byte Array in a pretty C array format. """
+    sys.stderr.write('Writing C Array to : ' + f.name + '\n\n')
+    
+    if options.get('head'):
+      f.write("/*\n" + options.get('head') + "*/\n\n")
+     
+    data_str = map(lambda x: "0x%02X" % x, bytedata)
+    f.write(options.get('name') + "_len = " + str(len(data_str)) + "; /* Num. of Bytes */\n")
+
+    # Print Bytedata
+    f.write(options.get('name') + "_data  = {\n")
+    
+    blq = data_str[:COLUMN]
+    i = 0
+    while i < len(data_str): 
+      f.write(', '.join(blq) + ',\n')
+      i += COLUMN
+      if i%32 == 0:
+        f.write('/*---------------- %8d ----------------*/\n' % i)
+
+      blq = data_str[i:min(i+COLUMN, len(data_str))]
+
+    f.write("}; \n")
+
+
+def IHEXoutput (bytedata, f, **options):
+  """ Write BTL Lib to a IHEX file. """
+  from intelhex import IntelHex
+  if options.get('head'):
+    print(options.get('head'))
+  
+  ih = options.get('IntelHex')        # IntelHex()
+  addr = options.get('addr')          # Address of sound data
+  ptr_addr = options.get('ptr_addr')  # Address of ptr to the end of sound data
+
+  ptr = len(bytedata) + addr
+  
+  # Writes the pointer in the header
+  ih[ptr_addr] = 0 # (ptr >> 24)
+  ptr = ptr & 0x00FFFFFF
+  
+  ptr_addr +=1
+  ih[ptr_addr] = ptr >> 16
+  ptr = ptr & 0x0000FFFF
+  
+  ptr_addr +=1
+  ih[ptr_addr] = ptr >> 8
+  ptr = ptr & 0x000000FF
+  
+  ptr_addr +=1
+  ih[ptr_addr] = ptr
+  
+  # Writes Data
+
+  for b in bytedata:
+    ih[addr] = b
+    addr +=1
+
+  # ih.write_hex_file(f)
+
+
+def force_stream_binary(stream):
+    """force binary mode for stream on windows. (take from intelhex module)"""
+    import os
+    if os.name == 'nt':
+        f_fileno = getattr(stream, 'fileno', none)
+        if f_fileno:
+            fileno = f_fileno()
+            if fileno >= 0:
+                import msvcrt
+                msvcrt.setmode(fileno, os.o_binary)
+
+
+def btloutputheader (bytedata, head, f):
+  """ writes to a btl library file Header. """
+  print(head)
+  
+  force_stream_binary(f)
+
+  # Writes the pointerr
+  f.write(chr(0))         # First byte it's cero
+
+  data_len = (1024 + len(bytedata)) & 0x00FFFFFF
+  f.write(chr(data_len >> 16))
+  data_len = data_len & 0x0000FFFF
+  
+  f.write(chr(data_len >> 8))
+  data_len = data_len & 0x000000FF
+  
+  f.write(chr(data_len))
+
+
+def BTLoutput (bytedata, head, f):
+  """ Writes to a BTL library file Data. """
+  
+  sys.stderr.write('Writing BTL binary Data to : ' + f.name + '\n\n')
+  force_stream_binary(f)
+  
+  for b in bytedata:
+    f.write(chr(b))
+
+
+
+class SoundsLib(object):
+  """ Creates a sound lib of BTc codec sounds """
+
+  def __init__(self, bitrate =22000, codec='BTc1.0', soft=24):
+    self.btc_codec  = codec     # Sound codec
+    self.bitrate    = bitrate   # BitRate
+    self.soft       = soft      # Desired softness constant
+    self.sounds     = {}  # Dict 'filename' : {inputwave, resultwave, bitstream, info}
+
+    self.r, self.c , self.info = CalcRC(self.bitrate, soft)
+
+  
+  def AddWAVSound(self, name):
+    """ Adds a WAV file to the sound library """
+    
+    if not name in self.sounds:
+      if not os.path.exists(name):
+        raise IOError ("File %s don't exists" % name)
+
+      sr, samples, info = ReadWAV(name)
+
+      # Resample to lib bitrate
+      if sr != self.bitrate:
+        samples, state = audioop.ratecv(samples, BYTES, 1, sr, self.bitrate, None)
+
+      name = name.split('.')[0]
+
+      self.sounds[name] = {'inputwave': samples, 'resultwave': None, \
+                                'bitstream': None, 'info': info}
+
+      return True
+    else:
+      return False
+
+
+  def DelSound(self, name):
+    del self.sounds[name]
+
+
+  def Process(self):
+    """ Process all sound with the desired codec and softness """
+    for name in self.sounds.keys():
+      if self.sounds[name]['resultwave'] is None: 
+        tmp, info = PredictiveBTC1_0 ( self.sounds[name]['inputwave'], self.soft)
+        self.sounds[name]['bitstream'] = tmp
+        self.sounds[name]['info'] += info
+
+
+  def PlayOriginal (self, name):
+    """ Plays Original sound if exists """
+    if name in self.sounds:
+      p = pyaudio.PyAudio() # Initiate audio system
+      Play(p, self.bitrate, self.sounds[name]['inputwave'])
+      p.terminate()
+ 
+  
+  def PlayProcesed (self, name):
+    """ Plays Procesed sound if exists """
+    if name in self.sounds:
+      p = pyaudio.PyAudio() # Initiate audio system
+
+      if self.sounds[name]['resultwave'] is None:
+         self.sounds[name]['resultwave'] = \
+             DecodeBTC1_0 (self.sounds[name]['bitstream'] , self.bitrate , self.r, self.c)
+
+      Play(p, self.bitrate, self.sounds[name]['resultwave'])
+      p.terminate()
+
+  
+  def WriteToFile (self, filename, outputFormat):
+    """ Write to a file the Sound Lib using a output format function """
+    # Ugly code here!
+    from intelhex import IntelHex
+
+    f = None
+    try:
+      if filename is None: # Try to open the file
+        f = sys.stdout
+      else:
+        if outputFormat == 'btl':
+          f = open(filename, 'wb')
+        else:
+          f = open(filename, 'w')
+
+      if outputFormat == 'btl_ihex':
+        ptr_addr = 0
+        addr = 1024
+        ih = IntelHex()
+
+      # Pre
+      if outputFormat == 'c':
+        f.write('/*\n' + self.info + '\t' + self.btc_codec + '\n*/\n')
+
+
+      # Generate packed data and write the header first
+      for name in self.sounds.keys():
+        data = BStoByteArray(self.sounds[name]['bitstream'])
+                
+        if outputFormat == 'c':
+          CArrayPrint(data, f, head= self.sounds[name]['info'], \
+              name=name);
+
+        elif outputFormat == 'btl_ihex':
+          IHEXoutput(data, f, IntelHex=ih, ptr_addr = ptr_addr, addr= addr, \
+              head= self.sounds[name]['info'] )
+          ptr_addr += 4
+          addr += len(data)
+          
+        elif outputFormat == 'btl':
+          print('k') 
+
+      # Post
+      if outputFormat == 'btl_ihex':
+        ih.write_hex_file(f)
+
+    finally:
+      if f != sys.stdout:
+        f.close()
+
+
+def ReadWAV (filename):
+  """ Reads a wave file and return sample rate and mono audio data """
 
   # Make header info
-  sys.stderr.write('Openining : ' + file + '\n\n')
-  wf = wave.open(file, 'rb')
+  sys.stderr.write('Openining : ' + filename + '\n\n')
+  wf = wave.open(filename, 'rb')
   
-  info = "\tWAV file: " + file + "\n"
+  info = "\tWAV file: " + filename + "\n"
   channels = wf.getnchannels()
   info += "\tOriginal Channels: " + str(channels)
   bits = wf.getsampwidth()
@@ -72,15 +296,16 @@ def ReadWAV (file):
   return sr, samples, info
 
 
-def Play(sr, samples):
+def Play(audio, sr, samples):
   """Plays an audio data
 
   Keywords arguments:
+  audio -- PyAduio Object
   sr -- Sample Rate
   samples -- Audio data in a string byte array (array.trostring())
 
   """
-  stream = p.open(format=p.get_format_from_width(BYTES), \
+  stream = audio.open(format=audio.get_format_from_width(BYTES), \
     channels=1, \
     rate=sr, \
     output=True)
@@ -98,7 +323,7 @@ def Play(sr, samples):
   stream.close()
 
 
-def PredictiveBTC1_0 ( samples, sr, soft):
+def PredictiveBTC1_0 ( samples, soft):
   """Encode audio data with BTc 1.0 audio codec. Returns a BitStream in a list
 
   Keywords arguments:
@@ -177,8 +402,7 @@ def CalcRC (br, soft, c=0.22*(10**-6)):
 
   r = -1.0 / (log(-1.0/soft +1)*br*c)
   
-  info = "\tR= " + fpformat.fix(r, 1) + " Ohms\tC = " + \
-        fpformat.fix((c/(10**-6)), 3) + "uF\n"
+  info =  "\tR= %d Ohms\tC = %.3f uF\n" % (r, c/(10**-6))
   info += "\tBitRate %(br)d\tSoftness constant = %(soft)d\n" % {'br': br, 'soft': soft}
 
   return r, c, info
@@ -204,125 +428,6 @@ def BStoByteArray (stream):
 
   return output
 
-def force_stream_binary(stream):
-    """Force binary mode for stream on Windows. (take from intelhex module)"""
-    import os
-    if os.name == 'nt':
-        f_fileno = getattr(stream, 'fileno', None)
-        if f_fileno:
-            fileno = f_fileno()
-            if fileno >= 0:
-                import msvcrt
-                msvcrt.setmode(fileno, os.O_BINARY)
-
-
-def CArrayPrint (bytedata, head, f, bias =0):
-    """ Prints a Byte Array in a pretty C array format. """
-    sys.stderr.write('Writing C Array to : ' + f.name + '\n\n')
-
-    f.write("/*\n" + head + "*/\n\n")
-     
-    data_str = map(lambda x: "0x%02X" % x, bytedata)
-    f.write("data_len = " + str(len(data_str)) + "; /* Num. of Bytes */\n")
-
-    # Print Bytedata
-    f.write("data = {\n")
-    blq = data_str[:COLUMN]
-    
-    i = 0
-    while i < len(data_str): 
-      f.write(', '.join(blq) + ',\n')
-      i += COLUMN
-      blq = data_str[i:min(i+COLUMN, len(data_str))]
-
-    f.write("}; \n")
-
-
-def RAWoutput (bytedata, head, f, bias =0):
-  """ Writes RAW binary Byte Array to a file. """
-  print(head)
-  
-  sys.stderr.write('Writing RAW binary to : ' + f.name + '\n\n')
-  force_stream_binary(f)
-  
-  for i in range (bias): # Desired padding
-    f.write(chr(0))
-
-  data_len = len(bytedata)
-  f.write(chr(data_len >> 24))
-  data_len = data_len & 0x00FFFFFF
-  
-  f.write(chr(data_len >> 16))
-  data_len = data_len & 0x0000FFFF
-  
-  f.write(chr(data_len >> 8))
-  data_len = data_len & 0x000000FF
-  
-  f.write(chr(data_len))
-
-  for b in bytedata:
-    f.write(chr(b))
-
-
-def BTLoutput (bytedata, head, f, bias =0):
-  """ Writes to a BTL library file. """
-  print(head)
-  
-  sys.stderr.write('Writing BTL binary to : ' + f.name + '\n\n')
-  force_stream_binary(f)
-  
-  for i in range (bias): # Desired padding
-    f.write(chr(0))
-
-  # Writes the header
-  f.write(chr(0))         # First byte it's cero
-
-  data_len = (1024 + len(bytedata)) & 0x00FFFFFF
-  f.write(chr(data_len >> 16))
-  data_len = data_len & 0x0000FFFF
-  
-  f.write(chr(data_len >> 8))
-  data_len = data_len & 0x000000FF
-  
-  f.write(chr(data_len))
-
-  for b in range(1020): # Fills the header
-    f.write(chr(0))
-
-  for b in bytedata:
-    f.write(chr(b))
-
-
-def IHEXoutput (bytedata, head, f, bias =0):
-  """ Write Byte Array to a IHEX file. """
-  from intelhex import IntelHex
-  print(head)
-  
-  ih = IntelHex()
-  
-  data_len = len(bytedata)
-
-  ih[bias] = (data_len >> 24)
-  data_len = data_len & 0x00FFFFFF
-  
-  bias +=1
-  ih[bias] = data_len >> 16
-  data_len = data_len & 0x0000FFFF
-  
-  bias +=1
-  ih[bias] = data_len >> 8
-  data_len = data_len & 0x000000FF
-  
-  bias +=1
-  ih[bias] = data_len
-  
-  bias +=1
-  for b in bytedata:
-    ih[bias] = b
-    bias +=1
-
-  ih.write_hex_file(f)
-
 
 # MAIN !
 if __name__ == '__main__':
@@ -342,16 +447,11 @@ if __name__ == '__main__':
       help='Softness constant. How many charge/discharge C in each time period.' + \
       ' Must be >2. Default: %(default)s ')
 
-  parser.add_argument('-f', '--format', choices=['c', 'btl', 'ihex', 'raw'], \
-      default='c', help='Output format. c -> C Array, ihex -> Intel IHEX, ' + \
-      'raw -> binary RAW, btl -> BotTalk Library. Default: %(default)s')
+  parser.add_argument('-f', '--format', choices=['c', 'btl', 'btl_ihex'], \
+      default='c', help='Output format. c -> C Array; ' + \
+      'btl -> BotTalk Library; ' + \
+      'btl_ihex -> BotTalk Library in IHEX format. Default: %(default)s')
 
-  # Maps output format wich options
-  formats = { 'c'     : CArrayPrint,
-              'raw'   : RAWoutput,
-              'ihex'  : IHEXoutput,
-              'btl'   : BTLoutput,
-              }
 
   parser.add_argument('-b', '--bias', metavar='N', type=int, default=0 , \
       help='Bias or Padding of the output file. In raw BIN insert N padding' +\
@@ -364,8 +464,6 @@ if __name__ == '__main__':
 
   args = parser.parse_args()
  
-  print(args)
-
   # Check input values
   if args.soft < 2:
     print("Invalid value of softness constant. Must be > 2.")
@@ -380,37 +478,25 @@ if __name__ == '__main__':
       print("The input file %s don't exists." % fname)
       sys.exit(0)
 
-  # Read WAV file and parses softness
-  sr, samples, info = ReadWAV(args.infile[0])
-  soft = args.soft
-
-  if args.playorig or args.p:
-    p = pyaudio.PyAudio() # Initiate audio system
+  sl = SoundsLib()
+  for f in args.infile:
+    sl.AddWAVSound(f)
 
   if args.playorig:
-    Play(sr, samples)     # Plays original audio
+    for k in sl.sounds.keys():
+      print("Playing Original Sound: " + k)
+      sl.PlayOriginal(k)
 
-  # Encode to BTc1.0
-  bitstream, coinfo = PredictiveBTC1_0(samples, sr, soft)
-  r, c, rcinfo = CalcRC(sr, soft)
+  # Process all sounds in the lib
+  sl.Process()
 
-  # Extra info for pretty output
-  info += coinfo + rcinfo
-  
+  # Play procesed sounds
   if args.p:
-    # Decodes BTc data to play it
-    output = DecodeBTC1_0(bitstream, sr, r, c)
-    Play(sr, output)
+    for k in sl.sounds.keys():
+      print("Playing Procesed Sound: " + k)
+      sl.PlayProcesed(k)
 
-  if args.playorig or args.p:
-    p.terminate()
-
-  data = BStoByteArray(bitstream)
-
-  #Apply output format
-  if args.output:
-    with open(args.output, 'wb') as f: 
-      formats[args.format](data, info, f, args.bias )
-  else:
-    formats[args.format](data, info, sys.stdout, args.bias )
+  # Write to output
+  sl.WriteToFile(args.output, args.format)
+  
 
