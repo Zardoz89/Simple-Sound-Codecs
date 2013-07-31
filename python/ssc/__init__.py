06b7a81a0c89177e6ac1913cab65819b7b565754
Luis Panadero Guardeño <luis.panadero@gmail.com>
2013-07-31 18:26:51 +0200

    Update function names from dm.py and removed import to configure.py

--- python/ssc/__init__.py
+++ python/ssc/__init__.py
@@ -4,7 +4,6 @@ Implementation of some simple and dumb audio codecs, like Delta Modualtion
 """
 
 from ssc.aux import pack, unpack
-from ssc.dm import predictive_dm, decode_dm
+from ssc.dm import lin2dm, dm2lin, calc_a_value
 from ssc.btc import lin2btc, btc2lin, calc_rc
-from ssc.configure import *
 
