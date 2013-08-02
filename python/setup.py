c0ad09a60b961e86ed26d120ac47ccf7c1c7411a
Luis Panadero Guardeño <luis.panadero@gmail.com>
2013-08-02 23:47:18 +0200

    Added centralized version control

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
+import re
+VERSIONFILE="ssc/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
 
 setup(
     name='ssc',
     description='Simple Sound Codecs like Delta Modulation, ADM, etc',
     license='BSD',
     keywords='sound codec DM ADM BTC',
-    version='0.0.1',
+    version=verstr,
     long_description=open('README.rst').read(),
 
     author='Luis Panadero Guardeño',
--- python/ssc/__init__.py
+++ python/ssc/__init__.py
@@ -6,4 +6,4 @@ Implementation of some simple and dumb audio codecs, like Delta Modualtion
 from ssc.aux import pack, unpack
 from ssc.dm import lin2dm, dm2lin, calc_a_value
 from ssc.btc import lin2btc, btc2lin, calc_rc
-
+from _version import __version__
